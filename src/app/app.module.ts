15801bd15c33d6db46f1db393798ec2e7fb58a9c
nadiandnd <ndtaeminnie@gmail.com>
2021-02-15 00:12:46 +0700

    ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducers/tutorial.reducer';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    ReadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({
+      tutorial: reducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
